204e58116629e168b2dfac41049d3742e610db19
CMLTTN <cemaldim@gmail.com>
2023-05-09 22:11:49 +0300

    get element by ıd

---
+++ app.js
@@ -0,0 +1,6 @@
+const progress = document.getElementById("progress");
+const circles= document.querySelectorAll(".circle");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+
+let currentActive = 1;
